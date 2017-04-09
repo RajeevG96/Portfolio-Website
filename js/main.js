d24d942efd2c88a3d6b02806ba8c52fba4658434
Rajeev Gill <gill96@live.co.uk>
2017-04-09 17:43:03 +0100

    Added jQuery

--- js/main.js
+++ js/main.js
@@ -1,35 +1,13 @@
-$(document).ready(function(){
-   
-    $("#seeMore").click(function(){
-       $("#primaryContent").hide(600);
-        $("#extraContent").show(600);
-    });
-    
-    $("#back").click(function(){
-        $("#extraContent").hide(600);
-         $("#primaryContent").show(600);
-    });
+var dropdownMenu = function(){
     
     $(".dropdown").click(function(){
-        $(".nav-container").slideDown(200);
-         $(".fa-level-down").hide();
-          $(".fa-level-up").show();
+       
+        $(".nav-container").toggle();
+    
     });
     
-//    $(".fa-level-up").click(function(){
-//        $(".nav-container").hide();
-//         $(".fa-level-up").hide();
-//          $(".fa-level-down").show();
-//    });
-    
-    $(".nav-container").on("click", function(e){
-    e.stopPropagation();
-    $(".nav-container:visible", $(this).siblings()).slideUp("fast");
-    $(".nav-container", this).slideDown();
-});
-    
-    $(document).click(function(){
-        $(".nav-container").slideUp(200);
-    });
-    
-});
+    var nav = document.getElementsByClassName("nav-container")
+    var downArrow = document.getElementsByClassName("")
+}
+
+$(document).ready(dropdownMenu);
