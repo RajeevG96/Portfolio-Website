851c8d51577833a0d5470d9bc22f6093364256ba
Rajeev Gill <gill96@live.co.uk>
2017-04-10 14:45:23 +0100

    Added mobile dropdown menu & Desktop styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-var dropdownMenu = function(){
-    
-    $(".dropdown").click(function(){
-       
-        $(".nav-container").toggle();
-    
-    });
-    
-    var nav = document.getElementsByClassName("nav-container")
-    var downArrow = document.getElementsByClassName("")
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-$(document).ready(dropdownMenu);
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
